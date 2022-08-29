1f34709ff043ffc10d17700858053127ca46d07f
yousi <yousifwilliam0123@gmail.com>
2022-08-29 15:38:59 -0400

    test

--- index.js
+++ index.js
@@ -1,12 +1,7 @@
 const express = require('express')
+const http = require("http")
 const app = express()
 const port = process.env.PORT || 3000
-
-/* 
-    Incase you are using mongodb atlas database uncomment below line
-    and replace "mongoAtlasUri" with your mongodb atlas uri.
-*/
-// mongoose.connect( mongoAtlasUri, {useNewUrlParser: true, useUnifiedTopology: true})
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
